a451f5feacd362d7192264d34482a737ebd0f8c8
Per Ekskog <per.ekskog@seb.se>
2023-01-17 15:32:32 +0100

    Generate two bundles.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 //import _ from "loadash";
 import "./style.css";
 import Icon from "./icon.png";
+import printMe from "./print";
 
 function component() {
   const element = document.createElement("div");
+  const btn = document.createElement("button");
 
   // Lodash, now imported by this script
+
   element.innerHTML = "Hello webpack";
   element.classList.add("hello");
+
+  btn.innerHTML = "Click me and check the console";
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
 
   // Add the image to our existing div.
   const myIcon = new Image();
